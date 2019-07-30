f2796acb4d09edd142d2a8748e01e5c3c52ca862
Antony Nganga <antonynganga05@gmail.com>
2019-07-30 20:07:53 +0300

    Add nav and landing components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
